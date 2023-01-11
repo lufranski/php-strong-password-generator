47e224668af423ebbe2f3259cf31fd949d79a115
Federico Lufrano <105553021+lufranski@users.noreply.github.com>
2023-01-11 14:44:53 +0100

    Milestone 1 - added some Bootstrap CSS

--- index.php
+++ index.php
@@ -6,17 +6,34 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Strong Password Generator</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+    <style>
+        body {
+            background-color: black;
+        }
 
+        h1,h3 {
+            color: #FB2576;
+        }
+
+        label {
+            color: #FB2576;
+        }
+    </style>
+    
     <?php
 
         $charNumber = $_GET['charnumber'];
 
-        var_dump($charNumber);
     ?>
 </head>
 <body>
+
+    <!-- Title -->
+    <div class="mx-auto my-5 w-50 text-center">
+        <h1>Strong Password Generator</h1>
+    </div>
     <!-- FORM -->
-    <form>
+    <form class="mx-auto my-3 w-50 text-center">
         <label for="charnumber">Type your password length:</label>
         <input type="number" name="charnumber">
         <input type="submit" value="VALIDATE">
@@ -28,9 +45,12 @@
             return substr(str_shuffle($data), 0, $charNumber);
         }
 
-        echo randomPswGen($charNumber);
+        echo '<div class="mx-auto my-3 w-50 text-center">' 
+        . '<h3>' 
+        . 'Your new Password is:' . ' ' . randomPswGen($charNumber) 
+        . '</h3>'
+        . '</div>';
 
-        // var_dump(strlen(randomPswGen($charNumber)))
     ?>
 </body>
 </html>
